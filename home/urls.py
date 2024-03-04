245a263842242c229926fb46a86d15816f671ba2
Khofi-Adjei007 <khofiadjei@gmail.com>
2024-03-04 23:22:55 +0000

    added landing login page

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 from django.urls import path
 
 urlpatterns = [
-    path('home_page/', views.home)
+    path('home_page/', views.home),
+    path('landing_page/', views.landing_page)
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home_page.html')
+
+
+def landing_page(request):
+    return render(request, 'landing_page.html')
